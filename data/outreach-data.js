e1ed1c63e5b442d608fab15f20ec27f4a9898fe7
kitamreik <kitdamreik@gmail.com>
2022-11-22 08:51:46 -0700

    updated data and complete

--- data/outreach-data.js
+++ data/outreach-data.js
@@ -1,15 +1,35 @@
 // DO NOT TOUCH
 const {v4: uuid} = require('uuid');
 
-let currentDate = new Date();
+// START outreach mod exports array
+module.exports = [
+    {
+        _id: uuid(),
+        firstName: "",
+        lastName: "",
+        email: "",
+        phoneNumber: "",
+        reason: ""
+    },
+    {
+        _id: uuid(),
+        firstName: "Test",
+        lastName: "Database Log",
+        email: "kit.tjclasses@gmail.com",
+        phoneNumber: "5555555555",
+        reason: "This is a fail-safe, hard coded entry to allow the database to capture the data here :]"
+    }
+];
 
-let outreachArray = [{
-    id: uuid(),
-    firstName: "",
-    lastName: "",
-    email: "",
-    phoneNumber: "",
-    reason: ""
-}]
+// let currentDate = new Date(); 
 
-module.exports = outreachArray;
+// CB
+/*
+let date = new Date();
+
+module.exports = {
+  userName: 'CodeSquader',
+  year: date.getFullYear(),
+  signedIn: true
+}
+*/
