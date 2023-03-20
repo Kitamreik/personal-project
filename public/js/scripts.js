0f5aa0c5a6bf4f685e0acbbb5e370dca9d34b354
kitamreik <kitdamreik@gmail.com>
2023-03-19 23:14:51 -0600

    setTimeout to welcome users to the site

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,12 @@
+// built in function for JS, used to set a timer 
+// method takes 2 parameters. One is a function, 2nd is delay --> a number, is optional
+setTimeout(() => {
+  // second, then run the message
+  alert("Hello and welcome to the website!");
+}, 6000); // first you wait 5 seconds --> 5000 milliseconds = 5 seconds
+
+
+
 // EXPLORE: https://stackoverflow.com/questions/63681387/how-do-i-transfer-data-from-console-log-to-a-json-file 
 
 // Hamburger Menu
