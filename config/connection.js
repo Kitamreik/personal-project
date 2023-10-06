dd43f23769a3c0056a662e66c697976039365bc1
kitamreik <kitdamreik@gmail.com>
2023-10-05 23:49:38 -0600

    mongoose upgrade in connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,26 +6,22 @@ const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
 //version 6- 5/23: mongoose downgrade works
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
-  if(!err) {
-    console.log("You have successfully connected to the MongoDB server! Hooray!");  
-  }
-  else {
-      console.log("ERROR- there is a connectivity issue with MongoDB. Please review.");
-      console.log("Server side errors occur when a failure to render a page falls on the server. A large spike in 500 or 503 errors could indicate the inability for the site's web hosting and server to manage the requirements of the site, resulting in downtime for visitors.");
-      console.log(err)
-  }
-});
+// mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+//   if(!err) {
+//     console.log("You have successfully connected to the MongoDB server! Hooray!");  
+//   }
+//   else {
+//       console.log("ERROR- there is a connectivity issue with MongoDB. Please review.");
+//       console.log("Server side errors occur when a failure to render a page falls on the server. A large spike in 500 or 503 errors could indicate the inability for the site's web hosting and server to manage the requirements of the site, resulting in downtime for visitors.");
+//       console.log(err)
+//   }
+// });
 
-/*
 // NEW- VERSION 7
 main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect(process.env.DB_URL)};
-
-
-*/
 
 
   
